04a364bc403ed6b68af0d0bb3f7bc81188a618a2
Jonathan Zheng <13380149+jxz12@users.noreply.github.com>
2025-01-10 21:17:42 +0000

    default is actually to not set this header

--- vJ0Er/config.h
+++ vJ0Er/config.h
@@ -32,9 +32,6 @@
 #undef MOUSEKEY_WHEEL_MAX_SPEED
 #define MOUSEKEY_WHEEL_MAX_SPEED 4
 
-// https://github.com/qmk/qmk_firmware/pull/23341/files
-#define MOUSEKEY_OVERLAP_RESET
-
 #define USB_POLLING_INTERVAL_MS 10
 #define SERIAL_NUMBER "vJ0Er/Ee4nYY"
 #define LAYER_STATE_8BIT
